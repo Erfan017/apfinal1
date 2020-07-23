f642f69b2b2059890d2f9ec629f6f744df63677f
Erfan017 <58765188+Erfan017@users.noreply.github.com>
2020-07-23 23:07:09 +0430

    shabe aval

---
+++ abcd/editrestaurantinfo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace abcd
+{
+    /// <summary>
+    /// Interaction logic for editrestaurantinfo.xaml
+    /// </summary>
+    public partial class editrestaurantinfo : Window
+    {
+        
+    
+    }
+}
